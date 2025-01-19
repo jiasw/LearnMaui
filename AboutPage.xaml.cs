1cdf3584d3324882f22f8fff75fb97c100372e59
jswjia <jswjia@163.com>
2025-01-19 23:46:02 +0800

    初始化

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiDemoApp;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+}
